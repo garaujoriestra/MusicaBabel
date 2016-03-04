6a79af6de4938afc0d36c418d90e81f62b801fcd
Guillermo Araujo Riestra <guillermo.araujo@babel.es>
2016-03-04 10:50:02 +0100

    Implementacion obtener valores del input del buscador y normalizar la info

--- js/buscador.js
+++ js/buscador.js
@@ -1,5 +1,24 @@
+//VARIABLES GLOBALES
+
+var filtro_palabras = [];
+
+function normalizarFiltro(datos){
+	console.log("Funcion NormalizarFiltro recibe : ", datos);
+	filtro_palabras = [];
+	var spliteado = datos.split(" ");
+	console.log(spliteado.length);
+	for (var i = 0; i < spliteado.length; i++) {
+		if(spliteado[i] != "")
+			filtro_palabras.push(spliteado[i]);
+	};
+
+	console.log(filtro_palabras);
+
+}
 $(document).ready(function(){   
-	$( ".filtro" ).keypress($.debounce(250, function(e) {
-	  console.log( "pulsado" );
+	$( ".filtro" ).keyup($.debounce(250, function(e) {
+	  console.log("texto",$(".filtro").val());
+	  var datos = $(".filtro").val();
+	  normalizarFiltro(datos);
 	}));
 });
